dd43c7bf99626b548c0530719edebb20bc058b1a
Emiko Johansson <kamonabe0@gmail.com>
2021-06-08 14:12:58 +0200

    redo att använda med tailwind.css

---
+++ tailwindcss_weather/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      Weather Application
+    </div>
+  );
+}
+
+export default App;
