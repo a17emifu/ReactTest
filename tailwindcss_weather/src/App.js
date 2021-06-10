877f2b5e4a5828be0d3f3dc31e9d7da89f17f99b
Emiko Johansson <kamonabe0@gmail.com>
2021-06-10 13:35:25 +0200

    demo 1 has completed

--- tailwindcss_weather/src/App.js
+++ tailwindcss_weather/src/App.js
@@ -3,10 +3,35 @@ import './App.css';
 import MakeWheatherCard from "./components/wheatherCard"
 
 function App() {
+  const cities = [
+    {
+      city_name: 'Tokyo',
+      color_name: 'blue'
+    },
+    {
+      city_name: 'London',
+      color_name: 'yellow'
+    },
+    {
+      city_name: 'Paris',
+      color_name: 'green'
+    }, 
+    {
+      city_name: 'Los Angeles',
+      color_name: 'red'
+    },
+  ];
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <MakeWheatherCard />
-    </div>
+    <div className="min-h-screen flex justify-center items-center flex-wrap">
+      {
+        cities.map((city,index) =>
+          <MakeWheatherCard
+          key={index}
+          city_name = {city.city_name}
+          color_name = {city.color_name}  />
+        )
+      }
+      </div>
   );
 }
 
--- tailwindcss_weather/src/components/wheatherCard.js
+++ tailwindcss_weather/src/components/wheatherCard.js
@@ -1,37 +1,62 @@
-function MakeWheatherCard(){
+import { useState, useEffect } from 'react';
+import dayjs from 'dayjs';
+
+
+function MakeWheatherCard({city_name, color_name}){
+    const[data, setData] = useState([]);
+    const[loading, setLoading] = useState(true);
+    useEffect(()=>{
+        fetch(`${process.env.REACT_APP_OW_API_URL}/weather/?q=${city_name}&APPID=${process.env.REACT_APP_OW_API_KEY}&units=metric`)
+        .then(res => res.json())
+        .then(result => {
+            console.log(result);
+          setData(result);
+          setLoading(false);
+        });
+      },[city_name]);
+      if(loading){
+          return <div>
+                  <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                  </svg>
+          </div>;
+      }
     return (
         <div className="p-4">
-            <div className ="w-96 h-56 m-auto bg-blue-500 rounded-xl shadow-2xl
-            transform hover:scale-110 transition-transform
-            text-white">
+            <div className={`bg-gradient-to-r from-${color_name}-500 to-${color_name}-300
+                w-96 h-56 m-auto rounded-xl shadow-2xl 
+                transform hover:scale-110 transition-transform
+                text-white`}>
                 <div className="flex flex-col w-full px-8 py-3">
                     <div className="flex justify-between">
                         <div className="flex flex-col ">
                             <span className="font-light">City Name</span>
-                            <span className="text-lg font-medium tracking-widest">Tokyo</span>
+                            <span className="text-lg font-medium tracking-widest">{data.name}</span>
                         </div>
                         <div>
-                            Image
+                        <img src={`${process.env.REACT_APP_OW_ICON_URL}/${data.weather[0].icon}.png`} 
+                             alt={data.weather[0].description}/>
                         </div>
                     </div>
                     <div className="pt-3">
                         <div className="flex flex-col ">
                             <span className="font-light">Wheather Condition</span>
-                            <span className="text-lg font-medium tracking-widest">Cloudy</span>
+                            <span className="text-lg font-medium tracking-widest"> { data.weather[0].main } </span>
                         </div>
                     </div>
                     <div className="pt-3 flex justify-between">
                         <div className="flex flex-col">
                             <span className="font-light">Date</span>
-                            <span className="font-medium tracking-widest text-xm">2021-06-10</span>
+                            <span className="font-medium tracking-widest text-xm">{ dayjs(data.ts).format('YYYY-MM-DD')}</span>
                         </div>
                         <div className="flex flex-col">
                             <span className="font-light">Temprature</span>
-                            <span className="text-xm font-medium tracking-widest">20℃</span>
+                            <span className="text-xm font-medium tracking-widest">{data.main.temp}℃</span>
                         </div>
                         <div className="flex flex-col">
                             <span className="font-light">Humidity</span>
-                            <span className="text-xm font-medium tracking-widest">20%</span>
+                            <span className="text-xm font-medium tracking-widest">{data.main.humidity}%</span>
                         </div>
                     </div>
                 </div>
